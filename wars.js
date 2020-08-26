ca01ed10f56d07274b8bc791ab73b66f24fbcf17
NyaradzoUBere <nybe5598@colorado.edu>
2020-08-25 22:33:26 -0600

    js average sln

---
+++ wars.js
@@ -0,0 +1,11 @@
+// 08.25.2020: Get the mean of an array 8ku
+
+// function getAverage(marks){
+//     //TODO : calculate the downwar rounded average of the marks array
+//     let total = 0
+//     for (let i = 0; i < marks.length; i++) {
+//       total += marks[i]
+//     }
+//     let avg = Math.floor(total / marks.length)
+//     return avg
+//   }
