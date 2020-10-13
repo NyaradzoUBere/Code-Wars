1b9b18cd73ef9116b676e35bdc29d075959bdaa1
NyaradzoUBere <nybe5598@colorado.edu>
2020-10-13 17:46:03 -0600

    daily practice

--- wars.js
+++ wars.js
@@ -575,39 +575,33 @@
 
 // maximum69Number(9996)
 
-// MAYBE
-// function setUnique(array) {
-//     let returnArray = []
-
-//     for (const element of array) {
-//         returnArray = unique(returnArray, element)
-//     }
-//     return returnArray
-// }
-
-// const unique = (returnArray, element, i = 1) => {
-//     if (returnArray.includes(element)) {
-//         if(returnArray.includes(element + 1)) {
-//             unique(returnArray, element, i + 1)
-//         } else {
-//             returnArray.push(element + i)
-//         }
-//     } else {
-//         returnArray.push(element)
-//     }
-//     return returnArray
-// }
-
-// console.log(setUnique(["apple", "banana", "apple", "banana"]))
-
 // SOLVED: List out transactions uniquely
-function setUnique(array) {
-    let sortedArray = array.sort()
-    let transactions = {}
-    for (let i = 0; i < sortedArray.length; i++) {
-        transactions[sortedArray[i]] = (transactions[sortedArray[i]] || 0) + 1
-    }
-    return transactions
-}
+// function setUnique(array) {
+//     let sortedArray = array.sort()
+//     let transactions = {}
+//     for (let i = 0; i < sortedArray.length; i++) {
+//         transactions[sortedArray[i]] = (transactions[sortedArray[i]] || 0) + 1
+//     }
+//     return transactions
+// }
 
-console.log(setUnique(["banana", "pear", "apple", "banana", "pear"]))
+// console.log(setUnique(["banana", "pear", "apple", "banana", "pear"]))
+
+// SOLVED: Maximum Product of Two Elements in an Array
+// function maxProduct(nums) {
+//     let firstMax = 0
+//     let secondMax = 0
+
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] > firstMax) {
+//             secondMax = firstMax
+//             firstMax = nums[i] - 1
+//         } else if (nums[i] > secondMax) {
+//             secondMax = nums[i] - 1
+//         }
+//     }
+//     return secondMax
+// }
+
+// console.log(maxProduct([3,4,5,2]))
+
